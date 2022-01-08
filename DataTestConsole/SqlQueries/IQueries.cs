a5946b588bd5d35bd53082db8e942e348bf6e629
paul <paul3ioan@gmail.com>
2022-01-08 00:06:32 -0800

    added

---
+++ DataTestConsole/SqlQueries/IQueries.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTestConsole.SqlQueries
+{
+    public interface IQueries
+    {
+        public object Query2();
+        public object Query3();
+        public object Query4();
+        public object Query5();
+        public object Query6();
+        public object Query7();
+        public object Query8();
+        public object Query9();
+        public object Query10();
+        public object Query11();
+        public object Query12();
+        public object Query13();
+        public object Query14();
+        public object Query15();
+    }
+}
